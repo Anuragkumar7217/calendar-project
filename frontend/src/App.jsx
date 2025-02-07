78bfea6561aa6f91d740487dcfd0c78129405526
Anurag Kumar <anuragkumar7217@gmail.com>
2025-02-07 10:45:48 +0530

    svg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,6 @@ import Header from "./components/Header";
 import Calendar from "./components/Calendar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />
