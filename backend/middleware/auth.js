9df7228417c78415d2246fa55fa382d6f78b2274
MohamedKamran-lab <mohamed.k@focusresearchlabs.com>
2025-03-21 10:33:58 +0530

    fix

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,7 +3,7 @@ const user = require("../models/User");
 
 const authMiddleware = async (req, res, next) => {
   // Get token from header
-  const token = req.header("x-auth-token");
+  const token = req.header("Authorization");
   // Check if no token
   if (!token) {
     return res.status(401).json({ msg: "No token, authorization denied" });
